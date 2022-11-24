1bbd21ad4704c0cc17ec81dfe2881fbba9a936f8
abdulkader-safi <safi.abdulkader@gmail.com>
2022-11-24 18:36:01 +0200

    Todo list in ReactJS

---
+++ src/components/Form.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const Form = ({ input, setInput, todos, setTodos }) => {
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const OnFormSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, {id: uuidv4(), title: input, completed: false}]);
+    setInput("");
+  };
+
+  return (
+    <form onSubmit={OnFormSubmit}>
+      <input
+        type="text"
+        placeholder="Enter a Todo..."
+        className="task-input"
+        value={input}
+        required
+        onChange={onInputChange}
+      />
+      <button className="button-add" type="submit">
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default Form;
